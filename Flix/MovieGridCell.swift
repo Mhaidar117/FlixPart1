b2323f4dddfbc71d17e832139455d5832dbb4ece
Michael Haidar <mhaidar@csumb.edu>
2019-09-23 09:57:18 -0700

    FlixPart2 commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Michael Haidar on 9/22/19.
+//  Copyright © 2019 Michael Haidar. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
